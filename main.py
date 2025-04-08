94cd5c72c25f42ceffd491183676e3f03dcd3837
Izdar Tohti <itohti@gmail.com>
2025-04-08 16:24:19 -0400

    saved firestore data locally

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import pandas as pd
 from src import data_loader
 from src import preprocess
 def main():
-    users, tasks = data_loader.get_data()
+    # users, tasks = data_loader.get_data()
+    users = pd.read_csv('./saved_data/users')
+    tasks = pd.read_csv('./saved_data/tasks')
     preprocess.preprocess(users, tasks)
 
 
