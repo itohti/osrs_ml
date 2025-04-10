a8c51489d22d97af36bdf3979eac11f99cc670f8
Izdar Tohti <97569757+itohti@users.noreply.github.com>
2025-04-10 17:39:23 -0400

    Resolves merge dataframes then engineer features

--- src/train.py
+++ src/train.py
@@ -1,11 +1,13 @@
 import pandas as pd
+from src import preprocess
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 
 def classification_model(player_name=None):
     df = pd.read_csv("./saved_data/merged_df.csv")
-
+    df = preprocess.feature_engineering(df)
+    df.to_csv("./saved_data/featured_merged_df.csv")
     # Convert "done" column to boolean
     df["done"] = df["done"].astype(bool).astype(int)
 
