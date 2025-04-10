1b65e3a08e9bfdf962c4024e859a65e0c19bed85
Izdar Tohti <itohti@gmail.com>
2025-04-10 19:01:13 -0400

    added potential_to_save_time

--- src/train.py
+++ src/train.py
@@ -7,8 +7,12 @@ from sklearn.metrics import classification_report
 def classification_model(player_name=None):
     df = pd.read_csv("./saved_data/merged_df.csv")
     df = preprocess.feature_engineering(df)
+
+    # just to see the featured merged df
     df.to_csv("./saved_data/featured_merged_df.csv")
     # Convert "done" column to boolean
     df["done"] = df["done"].astype(bool).astype(int)
+
+    
 
     
